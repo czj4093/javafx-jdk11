ae8622a42fa14389dd2d499dd3112d2a7415e7de
chenzejin <czj4093@qq.com>
2019-10-10 11:51:03 +0800

    配置文件改为json

--- src/main/java/sample/JFXMain.java
+++ src/main/java/sample/JFXMain.java
@@ -1,5 +1,7 @@
 package sample;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -9,72 +11,35 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.bson.Document;
 
-import java.io.FileInputStream;
-import java.util.Properties;
 import java.util.function.Consumer;
 
-/**
- * {
- *  "bookSourceName":"书源名称",
- *  "bookSourceGroup":"书源分组",
- *  "bookSourceUrl":"书源URL",
- *  "loginUrl":"登录URL",
- *  "checkUrl":"检测URL",
- *  "httpUserAgent":"User-Agent",
- *  "ruleFindUrl":"发现规则",
- *  "ruleSearchUrl":"搜索地址",
- *  "ruleSearchList":"搜索结果列表规则",
- *  "ruleSearchName":"搜索结果书名规则",
- *  "ruleSearchAuthor":"搜索结果作者规则",
- *  "ruleSearchCoverUrl":"搜索结果封面规则",
- *  "ruleSearchKind":"搜索结果分类规则",
- *  "ruleSearchLastChapter":"搜索结果最新章节规则",
- *  "ruleSearchNoteUrl":"搜索结果书籍URL规则",
- *  "ruleBookUrlPattern":"书籍详情URL正则",
- *  "ruleBookName":"书名规则",
- *  "ruleBookAuthor":"作者规则",
- *  "ruleCoverUrl":"封面规则",
- *  "ruleBookKind":"分类规则",
- *  "ruleBookLastChapter":"最新章节规则",
- *  "ruleChapterUrl":"目录URL规则",
- *  "ruleChapterUrlNext":"章节列表下一页规则",
- *  "ruleIntroduce":"简介规则",
- *  "ruleChapterList":"目录列表规则",
- *  "ruleChapterName":"章节名规则",
- *  "ruleContentUrl":"章节URL规则",
- *  "ruleContentUrlNext":"正文下一页URL规则",
- *  "ruleBookContent":"正文规则"}
- */
 public class JFXMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Properties prop = new Properties();
-        prop.load(getClass().getResourceAsStream("/filedConfig.properties"));
-        if (prop.size() > 0) {
-            System.out.println(prop.size());
-            stop();
-        }
+        JSONObject fieldConfig = JSON.parseObject(getClass().getResourceAsStream("/fieldConfig.json"), JSONObject.class);
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase mongoDatabase = mongoClient.getDatabase("hbck");
         MongoCollection<Document> collection = mongoDatabase.getCollection("bookSource");
         FindIterable<Document> documents = collection.find();
-        VBox vBox = new VBox();
+        TreeItem<String> rootItem = new TreeItem<>();
+        rootItem.setExpanded(true);
         documents.forEach((Consumer<? super Document>) d -> {
-            Label label = new Label("bookSourceName1");
-            TreeItem<String> rootItem = new TreeItem<>(d.getString("bookSourceName"), label);
-            d.forEach((k, v) -> rootItem.getChildren().add(new TreeItem<String>(v.toString(), new Label(k))));
-            TreeView<String> treeView = new TreeView<>(rootItem);
-            System.out.println("treeView height "+treeView.getHeight());
-            vBox.getChildren().add(treeView);
+            Label label1 = new Label(d.getString("bookSourceName"));
+            TreeItem<String> treeItem1 = new TreeItem<>(d.getString("bookSourceName"), label1);
+            rootItem.getChildren().add(treeItem1);
+            d.forEach((k, v) -> {
+                if (!k.equals("_id")) {
+                    treeItem1.getChildren().add(new TreeItem<>(v.toString(), new Label(fieldConfig.getString(k))));
+                }
+            });
         });
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(vBox, 300, 275));
+        TreeView<String> treeView = new TreeView<>(rootItem);
+        primaryStage.setTitle("书源");
+        primaryStage.setScene(new Scene(treeView, 500, 300));
         primaryStage.show();
     }
 
